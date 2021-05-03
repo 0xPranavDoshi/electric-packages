486f211890834e5d202433a2d37cae90d1e42a30
XtremeDevX <xtremedevx@gmail.com>
2021-05-03 07:39:47 +0400

    Fixed Auto-Updater

--- packages/config/auto-update.py
+++ packages/config/auto-update.py
@@ -1,7 +1,11 @@
-
 import os
 os.chdir(r'C:\Users\xtrem\Desktop\Electric\Electric Packages\packages')
 for package in os.listdir(r'C:\Users\xtrem\Desktop\Electric\Electric Packages\packages'):
     if package.endswith('.json'):
         os.system(f'au update {package}')
+    os.system('git add .')
+    os.system(f'git commit -m "Updated {package} to Latest Version"')
+
+os.system('git add .')
+os.system('git commit -m "Updated Packages"')
 os.system('git push -u origin master')
