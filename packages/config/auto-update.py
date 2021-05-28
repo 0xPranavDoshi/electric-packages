33350ceb9fdd5ea9680ca538ae79f6444c03e080
XtremeDevX <xtremedevx@gmail.com>
2021-05-28 07:24:57 +0400

    Updated 1-password.json to Latest Version

--- packages/config/auto-update.py
+++ packages/config/auto-update.py
@@ -1,6 +1,7 @@
 import os
 os.chdir(r'C:\Users\xtrem\Desktop\Electric\Electric Packages\packages')
 for package in os.listdir(r'C:\Users\xtrem\Desktop\Electric\Electric Packages\packages'):
+    print(package)
     if package.endswith('.json'):
         os.system(f'au update {package}')
     os.system('git add .')
